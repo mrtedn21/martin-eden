7c9945ef590582c41f0294740f367bdbaf7714d5
mrtedn21 <bezgin.sasha06@gmail.com>
2023-07-26 12:18:51 +0700

    refactor event loop

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import functools
 import selectors
 import socket
 from selectors import BaseSelector
-
-
-registered_coroutines = []
+from collections.abc import Coroutine
+from typing import Callable
 
 
 class Future:
@@ -36,50 +35,80 @@ class Future:
                 return self.result()
 
 
-def accept_connection(future: Future, connection: socket):
-    client_socket, client_address = connection.accept()
-    client_socket.setblocking(False)
-    future.set_result((client_socket, client_address))
+class EventLoop:
+    def __init__(self):
+        self._selector: BaseSelector = selectors.DefaultSelector()
+        self._coroutines: list[Coroutine] = []
 
+    def add_coroutine(self, coroutine: Coroutine):
+        self._coroutines.append(coroutine)
 
-def receive_data(future: Future, client_socket: socket):
-    result = b''
-    while True:
+    @staticmethod
+    def accept_connection_callback(future: Future, server_socket: socket.socket):
+        client_socket, client_address = server_socket.accept()
+        print(f'new client with address: {client_address}')
+        client_socket.setblocking(False)
+        future.set_result(client_socket)
+
+    @staticmethod
+    def receive_data_callback(future: Future, client_socket: socket.socket):
+        result = b''
+        while True:
+            try:
+                data = client_socket.recv(3)
+            except BlockingIOError:
+                break
+            result += data
+        print(f'new message from client: {result}')
+        future.set_result(result)
+
+    def _register_socket(self, sock: socket.socket, callback: Callable):
+        future = Future()
         try:
-            data = client_socket.recv(3)
-        except BlockingIOError:
-            break
-        result += data
-    print(result)
-    future.set_result(result)
+            self._selector.get_key(sock)
+        except KeyError:
+            self._selector.register(
+                sock,
+                selectors.EVENT_READ,
+                functools.partial(callback, future),
+            )
+        else:
+            self._selector.modify(
+                sock,
+                selectors.EVENT_READ,
+                functools.partial(callback, future),
+            )
+        return future
+
+    def register_server_socket(self, sock: socket.socket):
+        return self._register_socket(sock, self.accept_connection_callback)
+
+    def register_client_socket(self, sock: socket.socket):
+        return self._register_socket(sock, self.receive_data_callback)
+
+    def run(self, coroutine: Coroutine):
+        while True:
+            try:
+                coroutine.send(None)
+                for coro in self._coroutines:
+                    coro.send(None)
+
+                events = self._selector.select()
+
+                for key, mask in events:
+                    callback = key.data
+                    callback(key.fileobj)
+            except StopIteration:
+                break
 
 
-def register_connection_in_selector(sel: BaseSelector, sock, callback) -> socket:
-    future = Future()
-    try:
-        sel.get_key(sock)
-    except KeyError:
-        sel.register(
-            sock,
-            selectors.EVENT_READ,
-            functools.partial(callback, future),
-        )
-    else:
-        sel.modify(
-            sock,
-            selectors.EVENT_READ,
-            functools.partial(callback, future),
-        )
-    return future
-
-
-async def listen_client(sel, client_socket):
+async def listen_client(event_loop: EventLoop, client_socket: socket.socket):
     while True:
-        client_connection_future = register_connection_in_selector(sel, client_socket, receive_data)
+        client_connection_future = event_loop.register_client_socket(client_socket)
         await client_connection_future
 
 
-async def main(sel: BaseSelector):
+async def main(event_loop: EventLoop):
     sock = socket.socket()
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -88,28 +117,12 @@ async def main(sel: BaseSelector):
     sock.setblocking(False)
 
     while True:
-        connection_future = register_connection_in_selector(sel, sock, accept_connection)
-        client_socket, client_address = await connection_future
+        connection_future = event_loop.register_server_socket(sock)
+        client_socket = await connection_future
 
-        global registered_coroutines
-        registered_coroutines.append(listen_client(sel, client_socket))
+        event_loop.add_coroutine(listen_client(event_loop, client_socket))
 
 
 if __name__ == '__main__':
-    selector = selectors.DefaultSelector()
-
-    coro = main(selector)
-
-    while True:
-        try:
-            state = coro.send(None)
-            for coroutine in registered_coroutines:
-                coroutine.send(None)
-
-            events = selector.select()
-
-            for key, mask in events:
-                callback = key.data
-                callback(key.fileobj)
-        except StopIteration as si:
-            break
+    loop = EventLoop()
+    loop.run(main(loop))
